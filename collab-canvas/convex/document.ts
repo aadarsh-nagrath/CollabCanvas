d25676f0b7c52d6d027c88cd768fb3210d42e8e5
Aadarsh Nagrath <anagrath1@gmail.com>
2025-01-29 20:11:07 +0530

    will get back

--- collab-canvas/convex/document.ts
+++ collab-canvas/convex/document.ts
@@ -2,20 +2,35 @@ import {v} from "convex/values";
 import {mutation, query} from "./_generated/server";
 import {Doc, Id} from  "./_generated/dataModel";
 
-export const get = query({
-    handler: async (ctx) => {
+export const getSidebar = query({
+    args: {
+        parentDocument: v.optional(v.id("documents")),
+    },
+    handler: async (ctx, args) => {
         const identity = await ctx.auth.getUserIdentity();
-        
         if(!identity) {
             throw new Error("User not logged in");
-        }       
-        
-        const document = await ctx.db.query("documents").collect();
+        }
 
-        return document;
-        // Later we will add index here
+        const userId = identity.subject;
+
+        //fetch documents using indexes
     }
 });
+
+
+// export const get = query({
+//     handler: async (ctx) => {
+//         const identity = await ctx.auth.getUserIdentity();
+//         if(!identity) {
+//             throw new Error("User not logged in");
+//         }
+//         const document = await ctx.db.query("documents").collect();
+
+//         return document;
+//         // Later we will add index here
+//     }
+// });
 
 
 export const create = mutation({
